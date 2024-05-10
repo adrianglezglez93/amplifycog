f391a273f5e31c9318cd892e7c826d7d6cf84b9c
Adrian Gonzalez <adrian@dtssolutions.com.mx>
2024-05-09 19:34:49 -0600

    Code V1

---
+++ react-amplify-example/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import SignUpContainer from './SignUpContainer';
+
+function App() {
+  return (
+    <SignUpContainer/>
+  );
+}
+
+export default App;
